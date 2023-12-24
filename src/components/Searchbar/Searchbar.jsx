ce8842f8fd3a2dc2808156289d79f1358a546bcf
tetiana22 <134863156+tetiana22@users.noreply.github.com>
2023-12-24 13:10:55 +0000

    add if for formsubmit

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -9,46 +9,46 @@ export class SubmitForm extends Component  {
         currentPage: 1,
     }
 
-
     receiveTextForSearch = text => {
-        this.setState({ textForSearch: text });
+      this.setState({ textForSearch: text });
     };
+    
     handleSearch = ({ currentTarget: { value } }) => {
-        this.setState({text: value.toLowerCase()})
-    }
-    handleSubmit = event => {
-        event.preventDefault();
-        if (this.state.text.trim() === '') {
-            return toast.warn('Ви не ввели текст для пошуку!');
-        } else {
-            this.setState({
-                text: this.state.text,
-                images: [],
-                currentPage: 1
-            });
-            this.props.onSubmit(this.state.text); 
-            this.setState({ text: '' });
-        }
-    };
-
-    render() {
-        return (
-            
-            <Form onSubmit={this.handleSubmit}>
-                <Input
-                    type="text"
-                    name="text"
-                    value={this.state.text}
-                    autoComplete="off"
-                    autoFocus
-                    placeholder="Search images and photos"
-                    onChange={this.handleSearch}
-                />
-                <BtnSearch type="submit">Search</BtnSearch>
- 
-            </Form>
-        
-        )
+      this.setState({ text: value.toLowerCase() });
     }
     
+    handleSubmit = (event) => {
+      event.preventDefault();
+
+      const { text } = this.state;
+  
+      if (text.trim() === "") {
+        return toast.warn("Ви не ввели текст для пошуку!");
+      } else {
+        if (text === this.state.textForSearch) {
+          return toast.warn(`Ви вже переглядаєте ${text}`);
+    
+        }
+        this.setState({ textForSearch: text, images: [], currentPage: 1 });
+        this.props.onSubmit(text);
+        this.setState({ text: "" });
+      }
+    };
+    
+    render() {
+      return (
+        <Form onSubmit={this.handleSubmit}>
+            <Input
+                type="text"
+                name="text"
+                value={this.state.text}
+                autoComplete="off"
+                autoFocus
+                placeholder="Search images and photos"
+                onChange={this.handleSearch}
+            />
+            <BtnSearch type="submit">Search</BtnSearch>
+        </Form>
+      )
+    }
 }
